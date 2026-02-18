ffe10bf575a15656b1e658b477ea4de18dd1c3ca
Petro <petrotymchuck@gmail.com>
2026-02-18 13:24:24 +0200

    fix task2

--- public/pages/task_2.php
+++ public/pages/task_2.php
@@ -20,11 +20,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Завдання 3</title>
+    <title>Завдання 2</title>
     <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-    <h1>Завдання №3</h1>
+    <h1>Завдання №2</h1>
 
     <div class="form-container">
         <?php if ($error): ?>
